628ab855a1921822f11fe13dcbf976fb746a7cf5
Daniel Wheeler <daniel.wheeler2@gmail.com>
2018-01-12 17:23:35 -0500

    Add python dependencies

--- default.nix
+++ default.nix
@@ -7,11 +7,25 @@ let jekyll_env = bundlerEnv rec {
     lockfile = ./Gemfile.lock;
     gemset = ./gemset.nix;
   };
+  python = import ./requirements.nix { inherit pkgs; };
 in
   stdenv.mkDerivation rec {
     name = "env";
     buildInputs = [
       jekyll_env
       python36
+      python36Packages.numpy
+      python36Packages.jupyter
+      python36Packages.pillow
+      python36Packages.numpy
+      python36Packages.toolz
+      python36Packages.pytest
+      python36Packages.bokeh
+      python36Packages.matplotlib
+      python36Packages.flake8
+      python.packages."progressbar2"
+      python.packages."pykwalify"
+      python.packages."pylint"
+      python.packages."vega"
     ];
   }
