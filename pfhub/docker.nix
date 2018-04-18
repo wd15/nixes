4aab433b4ca3016bad1204ab891f1478d47f8840
Daniel Wheeler <daniel.wheeler2@gmail.com>
2018-04-18 17:05:21 -0400

    Make pfhub installable

--- pfhub/docker.nix
+++
@@ -1,9 +0,0 @@
-let
-inherit (import <nixpkgs> {}) fetchFromGitHub;
-nixes = fetchFromGitHub {
-  owner = "wd15";
-  repo = "nixes";
-  rev = "7861bfb99a327cd4027207f10939387844fc8f87";
-  sha256 = "0kaq0p30drqfs50xb9dwwd4pkdscm9kb4yb74w3l3wh5grzrnci6";
-  };
-in import "${nixes}/pfhub/default.nix"
