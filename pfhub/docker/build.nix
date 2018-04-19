19648fdafcaec11d1deaf2f650283b54b6e0909b
Daniel Wheeler <daniel.wheeler2@gmail.com>
2018-04-19 10:06:43 -0400

    Fix formatting

--- pfhub/docker/build.nix
+++ pfhub/docker/build.nix
@@ -6,4 +6,5 @@ let
     rev = "74a1109354171c7697eda75e1ec840049ade9ee6";
     sha256 = "1g15pr47hs6f002v2xa2pcx5bhfcf52f5vrv538c115w6hc5yzxl";
   };
-in import "${nixes}/pfhub/build.nix"
+in
+  import "${nixes}/pfhub/build.nix"
