9330334f5d715f0007d57428bdcf3f9889be0f8e
Daniel Wheeler <daniel.wheeler2@gmail.com>
2020-03-23 21:30:26 -0400

    torch stuff

--- torch/shell.nix
+++ torch/shell.nix
@@ -3,8 +3,8 @@ let
   nixpkgs_download = fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "5e46d5a7b59d69e7b86239195f58d26277ae0370";
-    sha256 = "0xynhyafvj8mazmagpnxp0k6l5wjfcg8j0syxvs4v87hf0g6n917";
+    rev = "793fc88dbe9c8f53b144fd970a3685f7b1dec729";
+    sha256 = "04g53i02hrdfa6kcla5h1q3j50mx39fchva7z7l32pk699nla4hi"; #20.03-beta
  };
   pkgs = import nixpkgs_download {
     config = {
@@ -13,9 +13,6 @@ let
     };
   };
   pythonPackages = pkgs.python3Packages;
-  pytorchWithCuda9 = pythonPackages.pytorchWithCuda.override {
-    cudatoolkit=pkgs.cudatoolkit_9_0;
-  };
 in
   pkgs.stdenv.mkDerivation rec {
     pname = "pytorch-play1";
@@ -31,11 +28,13 @@ in
       flake8
       black
       ipywidgets
-      pytorchWithCuda9
-      pkgs.cudatoolkit_9_0
+      pytorch
+      torchvision
+      pillow
+      pkgs.cudatoolkit
     ];
     src = null;
-    ##src=builtins.filterSource (path: type: type != "directory" || baseNameOf path != ".git") ./.; 
+    ##src=builtins.filterSource (path: type: type != "directory" || baseNameOf path != ".git") ./.;
     shellHook = ''
       jupyter nbextension install --py widgetsnbextension --user > /dev/null 2>&1
       jupyter nbextension enable widgetsnbextension --user --py  > /dev/null 2>&1
@@ -52,7 +51,9 @@ in
       export PATH=$PATH:$PYTHONUSERBASE/bin
 
       export CUDAPATH="/usr/lib/x86_64-linux-gnu"
-      export LD_PRELOAD=$CUDAPATH/libcuda.so.1:$CUDAPATH/libnvidia-fatbinaryloader.so.390.116:$CUDAPATH/libnvidia-ptxjitcompiler.so.1:$CUDAPATH/libnvidia-ml.so.1
+      export LD_PRELOAD=$CUDAPATH/libcuda.so.435.21:$CUDAPATH/libnvidia-fatbinaryloader.so.435.21:$CUDAPATH/libnvidia-ptxjitcompiler.so.435.21:$CUDAPATH/libnvidia-ml.so.435.21
+      export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+
       # To install extra packages use
       #
       # $ pip install --user <package>
